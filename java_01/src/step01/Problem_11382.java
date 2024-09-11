2e0fae41fa1b6ebd525d9c9213119f371103c7c3
codemyungja <codemyungja@naver.com>
2024-09-11 21:40:17 +0900

    baekjoon : 문제 11382

---
+++ java_01/src/step01/Problem_11382.java
@@ -0,0 +1,20 @@
+package step01;
+
+import java.util.Scanner;
+//꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+//입력 : 첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 10^12)이 공백을 사이에 두고 주어진다.
+public class Problem_11382 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine(); // 77 77 7777
+		String[] arr = str.split(" ");
+		long[] numArr = new long[3];
+		
+		for(int i = 0; i < arr.length; i++) {
+			numArr[i] = Long.parseLong(arr[i]);
+		}
+		
+		System.out.println(numArr[0] + numArr[1] + numArr[2]);
+	
+	}
+}
