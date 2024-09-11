6f693e12480ca7000c9a685b5c4ad141ee63a5bc
codemyungja <codemyungja@naver.com>
2024-09-11 20:58:24 +0900

    baekjoon : 문제 10430

---
+++ java_01/src/java_01/abstract_interface/AbstractDemo.java
@@ -0,0 +1,20 @@
+package java_01.abstract_interface;
+
+abstract class A1 {
+	public abstract void test();
+}
+
+abstract class A2 extends A1 {
+	// 추상 클래스간의 상속에서는 구현의 의무가 발생하지 않는다.
+}
+
+
+
+
+public class AbstractDemo {
+	
+	
+	
+	
+	
+}
