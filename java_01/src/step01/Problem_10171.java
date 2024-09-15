8320337cc8c96baa5a60e4e1d7470de78b1a1148
codemyungja <codemyungja@naver.com>
2024-09-15 20:34:04 +0900

    baekjoon : 문제 10171

---
+++ java_01/src/step01/Problem_10171.java
@@ -0,0 +1,12 @@
+package step01;
+
+public class Problem_10171 {
+	public static void main(String[] args) {
+		// valid ones are  \b  \t  \n  \f  \r  \"  \'  \\ 
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
