6a8ff178f56c16f176c3ee476b77489341971a29
codemyungja <codemyungja@naver.com>
2024-09-15 21:22:26 +0900

    baekjoon : 문제2884

---
+++ java_01/src/step02/Problem_2884.java
@@ -0,0 +1,22 @@
+package step02;
+import java.util.Scanner;
+
+public class Problem_2884 {
+	public static void main(String[] agrs){
+		Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int min = sc.nextInt();
+		
+		if(min >= 45){ // 45분 이상일 때
+			System.out.println(hour + " " + (min-45));
+		} else{ // 45분 미만일 때 -> 시간을 1줄여야함
+			int resultMin = 60 + min - 45;
+			int resultHour = hour - 1;
+			if(resultHour >= 0){
+				System.out.println(resultHour + " " + resultMin);
+			} else{ // 이전날로 계산해야함
+				System.out.println("23 " + resultMin);
+			}
+		}
+	}
+}
