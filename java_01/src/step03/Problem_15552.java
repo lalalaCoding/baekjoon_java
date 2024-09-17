8f94b9ca7fb3d4d8b3d2c253456523e75e863507
codemyungja <codemyungja@naver.com>
2024-09-17 21:16:58 +0900

    baekjoon : 문제 15552, 25314

---
+++ java_01/src/step03/Problem_15552.java
@@ -0,0 +1,28 @@
+package step03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem_15552 {
+	public static void main(String[] args){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 콘솔을 통해 입력받음
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 콘솔을 통해 출력함
+		
+		
+		try {
+			int t = Integer.parseInt(br.readLine());
+			for(int i = 0; i < t; i++){
+				String[] arr = br.readLine().split(" ");
+				int sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+				bw.write(sum + "\n");
+			}
+			bw.flush(); // for문 밖에서 1번만 실행해도 됨. -> for문 안에서 하면 메모리 낭비임
+			bw.close();
+			br.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ java_01/src/step03/Problem_25314.java
@@ -0,0 +1,16 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Problem_25314 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		StringBuffer sb = new StringBuffer("long");
+		for(int i = 1; i < n/4; i++){
+			sb.append(" long");
+		}
+		sb.append(" int");
+		System.out.println(sb);
+	}
+}
