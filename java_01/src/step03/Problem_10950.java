4e0e24aee81b0517e7ee07a54df5239fb9639d30
codemyungja <codemyungja@naver.com>
2024-09-17 20:18:56 +0900

    baekjoon : 문제 10950

---
+++ java_01/src/step03/Problem_10950.java
@@ -0,0 +1,16 @@
+package step03;
+
+import java.util.Scanner;
+
+public class Problem_10950 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		for(int i = 0; i < t; i++){
+			String[] arr = sc.nextLine().split(" ");
+			System.out.println(Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+		}
+		
+	}
+}
