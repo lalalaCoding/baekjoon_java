cc5fd0f3eebf2c31d9c5dd41ea36c180e6ccd54c
codemyungja <codemyungja@naver.com>
2024-09-17 19:41:39 +0900

    IO 복습

---
+++ java_01/src/java_01/FIleDemo.java
@@ -0,0 +1,13 @@
+package java_01;
+
+import java.io.File;
+
+public class FIleDemo {
+	public static void main(String[] args) {
+		File file = new File("C:\\dev/readme.txt");
+		System.out.println(file.getName());
+		System.out.println(file.getParent()); // C:\dev
+		System.out.println(file.getAbsolutePath());
+		System.out.println(file.getPath());
+	}
+}
