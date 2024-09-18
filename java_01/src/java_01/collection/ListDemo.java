e63624db738fe71ac887cb6951dc116a1911b70a
codemyungja <codemyungja@naver.com>
2024-09-18 18:53:50 +0900

    java List 복습

---
+++ java_01/src/java_01/collection/ListDemo.java
@@ -0,0 +1,15 @@
+package java_01.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("개나리");
+		list.add("소나무");
+		list.add("진달래");
+		List<String> subList = list.subList(1, 2); // 1 <= index < 2
+		System.out.println(subList);
+	}
+}
