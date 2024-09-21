cacbffa43a5b60b7cc5e3f53bb62fd5f3dab677b
codemyungja <codemyungja@naver.com>
2024-09-21 21:49:50 +0900

    baekjoon : 문제 10809

--- java_01/src/java_review/Index.java
+++ java_01/src/java_review/Index.java
@@ -1,15 +1,26 @@
 package java_review;
 
-import java_01.array.ArrayCopy;
-import java_01.scope.ScopeDemo;
+import java.util.StringTokenizer;
 
 public class Index {
 	public static void main(String[] args) {
 		
-		ArrayCopy arrayCopy = new ArrayCopy();
-		arrayCopy.arrayCopy();
+		//ArrayCopy arrayCopy = new ArrayCopy();
+		//arrayCopy.arrayCopy();
 		
-		ScopeDemo scope = new ScopeDemo();
+		//ScopeDemo scope = new ScopeDemo();
+		
+		String str = "abc";
+		String[] arr = str.split(""); // 한글자씩 자를 수 있음!
+		System.out.println(arr[0]);
+		
+		StringTokenizer st = new StringTokenizer(str, "[]");
+		System.out.println(st.nextToken()); // 한글자씩 자를 수 없음
+		
+		int aCode = 'a';
+		System.out.println(aCode);
+		char a = (char)aCode;
+		System.out.println(a);
 		
 		
 		
